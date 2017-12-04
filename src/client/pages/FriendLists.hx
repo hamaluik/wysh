04de3656f848b0109aba54bf671cd0853f7af415
Kenton Hamaluik <kentonh@gmail.com>
2017-12-03 22:40:13 -0700

    added random quotes as a placeholder

--- src/client/pages/FriendLists.hx
+++ src/client/pages/FriendLists.hx
@@ -6,8 +6,33 @@ import components.ListSelector;
 class FriendLists implements Mithril {
     public function new() {}
 
+    var quote:Null<String> = null;
+    var author:Null<String> = null;
+
     public function onmatch(params:haxe.DynamicAccess<String>, url:String) {
-        if(Store.state.auth.token == null) M.routeSet('/');
+        if(Store.state.auth.token == null) { M.routeSet('/'); return null; }
+
+        quote = null;
+        M.redraw();
+        M.jsonp({
+            url: 'http://api.forismatic.com/api/1.0/',
+            data: {
+                method: 'getQuote',
+                format: 'jsonp',
+                lang: 'en',
+                jsonp: 'callback'
+            },
+            callbackKey: "jsonp"
+        })
+        .then(function(q:Dynamic) {
+            quote = q.quoteText;
+            author = q.quoteAuthor;
+            M.redraw();
+        })
+        .catchError(function(error) {
+            js.Browser.console.error('quote error', error);
+        });
+
         return null;
     }
 
@@ -18,8 +43,14 @@ class FriendLists implements Mithril {
                 m('.container', [
                     m('.columns', [
                         m(ListSelector, { type: Friends }),
-                        m('.column', [
-                            m('p', 'Your friends don\'t have any lists!')
+                        m('.column.content', [
+                            quote == null ? m('.loading-bar') : m('blockquote', [
+                                m('p', quote),
+                                m('footer.has-text-right', [
+                                    m('span', '— '),
+                                    m('cite', (author != null && StringTools.trim(author).length > 0) ? author : 'Unknown')
+                                ])
+                            ])
                         ]),
                     ])
                 ])
